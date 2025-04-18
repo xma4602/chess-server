a79843f8a8e647d6c448d7d952cbc03292ddcbb3
xma4602 <xma4602@gmail.com>
2025-04-18 16:40:37 +0400

    init project

---
+++ src/test/java/com/chess/server/ChessServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chess.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
