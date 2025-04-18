186343bd1cea71927d921339395ef905613332d7
Petros607 <markaryan.petr@mail.ru>
2025-04-18 23:03:32 +0400

    Эхо бот на java

---
+++ src/main/java/com/chess/bot/ChessBot.java
@@ -0,0 +1,63 @@
+package com.chess.bot;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.CopyMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class ChessBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return "ChessBotTest";
+    }
+
+    @Override
+    public String getBotToken() {
+        return System.getenv("BOT_TOKEN");
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+//        System.out.println(update);
+        var msg = update.getMessage();
+        var user = msg.getFrom();
+        var userId = user.getId();
+
+        System.out.println(userId + "  " + user.getUserName() + " wrote " + msg.getText());
+        sendText(userId, msg.getText());
+        copyMessage(userId, msg.getMessageId());
+    }
+
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        ChessBot bot = new ChessBot();
+        botsApi.registerBot(bot);
+        bot.sendText(837823253L, "Я проснулся!");
+    }
+
+    public void sendText(Long who, String what){
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString()) //Who are we sending a message to
+                .text(what).build();    //Message content
+        try {
+            execute(sm);                        //Actually sending the message
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);      //Any error will be printed here
+        }
+    }
+
+    public void copyMessage(Long who, Integer msgId){
+        CopyMessage cm = CopyMessage.builder()
+                .fromChatId(who.toString())  //We copy from the user
+                .chatId(who.toString())      //And send it back to him
+                .messageId(msgId)            //Specifying what message
+                .build();
+        try {
+            execute(cm);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/com/chess/bot/main.java
+++
@@ -1,4 +0,0 @@
-package com.chess.bot;
-
-public class main {
-}
