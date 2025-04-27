a5b4484461f2054511f98412c902c0054c36e0b7
Petros607 <markaryan.petr@mail.ru>
2025-04-27 18:09:17 +0400

    Соединил front и бэк 3

--- frontend/src/data.service.ts
+++ frontend/src/data.service.ts
@@ -1,7 +1,8 @@
 const http = 'https://';
 const ws = 'ws://';
 const serverHost = 'chess-server-gvgy.onrender.com:8080';
-const frontHost = 'chess-frontend-wpc3.onrender.com:4200';
+// const frontHost = 'chess-frontend-wpc3.onrender.com:4200';
+const frontHost = 'localhost:4200';
 const appPrefix = '/chess';
 
 https://chess-frontend-wpc3.onrender.com
