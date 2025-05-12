1f17caa161889a0150025209ecc81603d2e406cb
Petros607 <markaryan.petr@mail.ru>
2025-05-12 19:24:12 +0400

    Версия для деплоя

--- src/main/java/com/chess/server/WebConfig.java
+++ src/main/java/com/chess/server/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Разрешить доступ ко всем URL
-                .allowedOrigins("https://chess-frontend-wpc3.onrender.com:4200") // Разрешите доступ только с этого домена
+                .allowedOrigins("https://chess-frontend-wpc3.onrender.com") // Разрешите доступ только с этого домена
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Разрешите необходимые методы
                 .allowedHeaders("*") // Разрешите все заголовки
                 .allowCredentials(true); // Разрешите отправку куки
